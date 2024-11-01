c1c72b6ab37431f63e467d08a061cdb83181447d
Maoyin Sun <simonmysun@gmail.com>
2024-11-02 00:09:38 +0100

    add more achievements

--- src/comment.js
+++ src/comment.js
@@ -12,28 +12,68 @@ const bSearch = (l, r, v, arr) => {
 
 const comments = [
   {
-    v: 0.1,
+    v: 0,
     c: 'Less than 10cm. You suck! ',
   },
   {
-    v: 0.3,
+    v: 0.1,
     c: 'You need some more courage. ',
   },
   {
-    v: 0.6,
+    v: 0.2,
     c: 'The first step to the addiction of throwing your phone(s). ',
   },
   {
-    v: 1.2,
+    v: 0.35,
     c: 'Ordinary. ',
   },
   {
-    v: 1.5,
+    v: 0.5,
     c: 'Fine, I see your effort. ',
   },
   {
-    v: 15,
+    v: 0.6,
+    c: 'Getting there, but not quite a pro yet!',
+  },
+  {
+    v: 0.8,
+    c: 'Impressive! You might want to consider a career in phone tossing.',
+  },
+  {
+    v: 1.0,
+    c: 'Wow! You are a phone throwing enthusiast!',
+  },
+  {
+    v: 1.5,
+    c: 'Incredible! Are you sure you are not using a catapult?',
+  },
+  {
+    v: 2,
     c: 'You must be crazy',
+  },
+  {
+    v: 3,
+    c: 'Are you trying to send your phone to space?',
+  },
+  {
+    v: 5,
+    c: 'This is not phone throwing, this is phone launching!',
+  },
+  {
+    v: 7,
+    c: 'Congratulations! You have reached the ultimate phone throwing achievement!',
+  },
+  {
+    v: 11,
+    c: 'Did you just break the sound barrier with your phone?',
+  },
+  {
+    v: 15,
+    c: 'Your phone is now a satellite!',
+  },
+  {
+    v: 20,
+    c: 'You have transcended phone throwing. You are now a legend!',
   },
 ];
 
